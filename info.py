130829768d88d016e6698cd4ce665081c40a4b1e
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2024-11-16 22:41:21 +0600

    Update info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movie_Request_Group_23')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AllBotUpdatemy')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowtoDowlnoad/3')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Ctgmovies28) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Ctgmovies28') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
